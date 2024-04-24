1d825b3d6b1d5c0743ffef93f854353f80e42a2b
dlybeck <dlybeck383@gmail.com>
2024-04-24 14:50:33 -0700

    Can get all data for lebron james and dump into JSON

--- get_data.py
+++ get_data.py
@@ -1,18 +1,27 @@
-#First we import the endpoints
-#We Will be using Pandas dataframes to manipulates the data
 from nba_api.stats.endpoints import playergamelog
 import pandas as pd
+from nba_api.stats.library.parameters import SeasonAll
+import json
 
-#Call the API endpoints passing in lebron's ID & which season
-gamelog_bron =playergamelog.PlayerGameLog(player_id='2544' , season='2018')
+# Call the API endpoints passing in LeBron's ID & which season
+gamelog_bron = playergamelog.PlayerGameLog(player_id='2544', season='2023')
 
-#Convert gamelog object into a pandas
-#Can Also Convert to JSON to dictionary
-df_bron_games_2018 = gamelog_bron.get_data_frames()
+# Convert gamelog object into a pandas DataFrame
+df_bron_games_2023 = pd.concat(gamelog_bron.get_data_frames())
 
-#if want all seasons, you must imort SeasonAll parameters
-from nba_api.stats.library.parameters import SeasonAll 
+# Get data for all seasons
+game_log_bron_all = playergamelog.PlayerGameLog(player_id='2544', season=SeasonAll.all)
+df_bron_games_all = pd.concat(game_log_bron_all.get_data_frames())
 
-game_log_bron_all = playergamelog.PlayerGameLog(player_id='2544' ,season=SeasonAll.all)
+# Convert the DataFrames to JSON
+json_data_2023 = df_bron_games_2023.to_json(orient='records')
+json_data_all = df_bron_games_all.to_json(orient='records')
 
-df__bron_games_all = game_log_bron_all.get_data_frames()
+# Write the JSON data to files
+with open('lebron_2023.json', 'w') as f:
+    json.dump(json_data_2023, f, indent=4)
+
+with open('lebron_all.json', 'w') as f:
+    json.dump(json_data_all, f, indent=4)
+
+print("JSON files created successfully!")
